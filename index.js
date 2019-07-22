f87b405c3683a5b6d97a54252c7b7da123526da3
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-07-22 02:03:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,21 +25,14 @@ function logWhisper(string){
 
 
 function sayHiToGrandma(string){
-  if string === shout(string){
-    return 
+  if (string === whisper(string)){
+    return ("I can't hear you!")
   }
   
-  
+  else if (string === shout(string))
+    return ("YES INDEED!")
 }
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
 
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
+  else if (string === "I love you, Grandma." ){
+    return "I love you, too."
+  }
